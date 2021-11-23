5a189950f0653a3261affd9a8deefa08c079953b
Alexandre Henrique <alexandrehenrique.es@gmail.com>
2021-11-23 14:50:13 -0400

    ✨ NEW FEATURE: Adicionado metodo para criar um novo usuario, realizar login e exibir os dados do mesmo

---
+++ src/store/user/actions.js
@@ -0,0 +1,51 @@
+import { api } from 'boot/axios'
+import f from "../../functions/index"
+
+// ROTAS NÃO AUTENTICADAS PELO TOKEN REALIZAM A REQUISIÇÃO COM A IMPORTAÇÃO DA VARIAVEL api
+export function createUser ({ commit }, { data }) {
+    return new Promise((resolve, reject) => {
+        api.post('/users', data).then((res) => {
+            resolve(res)
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+
+export function loginUser ({ commit }, { data }) {
+    return new Promise((resolve, reject) => {
+        api.post('/authentication/login', data).then((res) => {
+            window.localStorage.setItem('marketplace_token', res.data.token)
+            commit('SET_TOKEN', res.data.token)
+            commit('SET_USER_DATA', res.data.user)
+            resolve(res)
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+// FIM ROTAS NÃO AUTENTICADAS
+
+// ROTAS AUTENTICADAS PELO TOKEN REALIZAM A REQUISIÇÃO COM A IMPORTAÇÃO DA VARIAVEL f
+export function getUser ({ commit }, { id }) {
+    return new Promise((resolve, reject) => {
+        f().axiosToken().get(`/users/${id}`).then((res) => {
+            commit('SET_USER_DATA', res.data)
+            resolve(res)
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+
+export function getUsers ({ commit }) {
+    return new Promise((resolve, reject) => {
+        f().axiosToken().get('/users/list').then((res) => {
+            commit('SET_LIST_USERS', res.data)
+            resolve(res)
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+// FIM ROTAS AUTENTICADAS
